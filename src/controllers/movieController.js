28eea9d418cc1578a64976d6aaab6b9d9f1653d9
James Junior <jamescosjr@gmail.com>
2024-10-14 18:44:58 -0300

    refactor: update movieController.js to use movieRepository and validation functions

--- src/controllers/movieController.js
+++ src/controllers/movieController.js
@@ -1,77 +1,133 @@
-import { registerMovie, listMovies, findMovieByTitle, listMoviesByDirector, listMoviesByGenre, listMoviesByYear, deleteMovieById, updateMovieById } from '../service/movieService.js';
+import {
+  register,
+  findAll,
+  findByTitle,
+  listByGenre,
+  listByDirector,
+  listByYear,
+  deleteById,
+  movies,
+  updateById,
+} from "../repository/movieRepository.js";
+import { validateMovie } from "../utils/validation.js";
 
-export function registerMovieHandler(data) {
-    try {
-        const movie = registerMovie(data);
-        console.log('Movie registered successfully:', movie);
-    } catch (error) {
-        console.error('Error registering movie:', error.message);
+export async function registerMovieHandler(req, res) {
+    const movie = req.body;
+    const isValid = validateMovie(movie);
+
+    if (!isValid) {
+        return res.status(400).json({ message: 'Invalid movie data' });
     }
-};
 
-export function listMoviesHandler() {
-    const movies = listMovies();
-    console.log('Movies:', movies);
-};
-
-export function findMovieByTitleHandler(title) {
-    const movie = findMovieByTitle(title);
-    if (!movie) {
-        console.log('Movie not found');
-        return;
-    }
-    console.log('Movie found:', movie);
-};
-
-export function listMoviesbyGenreHandler(genre) {
-    try {
-        const movies = listMoviesByGenre(genre);
-        console.log('Movies found:', movies);
-    } catch (error) {
-        console.error('Error listing movies by genre:', error.message);
-    }
+  try {
+    const newMovie = await register(movie);
+    res.status(201).json(newMovie);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+  
 }
 
-export function listMoviesByDirectorHandler(director) {
-    try {
-        const movies = listMoviesByDirector(director);
-        console.log('Movies found:', movies);
-    } catch (error) {
-        console.error('Error listing movies by director:', error.message);
-    }
+export function listMoviesHandler(req, res) {
+  try {
+    const movies = findAll();
+    res.status(200).json(movies);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+ 
 }
 
-export function listMoviesByYearHandler(year) {
-    try {
-        const movies = listMoviesByYear(year);
-        console.log('Movies found:', movies);
-    } catch (error) {
-        console.error('Error listing movies by year:', error.message);
-    }
+export function findMovieByTitleHandler(req, res) {
+  const title = req.params.title;
+  
+  try {
+      const movie = findByTitle(title);
+      
+      if (!movie) {
+          return res.status(404).json({ message: "Movie not found" });
+      }
+      
+      res.status(200).json(movie);
+  } catch (error) {
+      res.status(500).json({ message: error.message });
+  }
 }
 
-export function deleteMovieHandler(id) {
-    try {
-        const deletedMovie = deleteMovieById(id);
-        if (!deletedMovie) {
-            console.log('Movie not found, nothing to delete.');
-            return;
-        }
-        console.log('Movie deleted successfully:', deletedMovie);
-    } catch (error) {
-        console.error('Error deleting movie:', error.message);
-    }
-};
 
-export function updateMovieHandler(id, data) {
+export function listMoviesbyGenreHandler(req, res) {
+  const genre = req.params.genre;
+
+  try {
+      const movies = listByGenre(genre);
+      
+      if (movies.length === 0) {
+          return res.status(404).json({ message: 'Genre not found' });
+      }
+
+      res.status(200).json(movies);
+  } catch (error) {
+      res.status(500).json({ message: error.message });
+  }
+}
+
+export function listMoviesByDirectorHandler(req, res) {
+  const director = req.params.director;
+
+  try {
+      const movies = listByDirector(director);
+
+      if (movies.length === 0) {
+          return res.status(404).json({ message: 'Director not found' });
+      }
+
+      res.status(200).json(movies);
+  } catch (error) {
+      res.status(500).json({ message: error.message });
+  }
+}
+
+export function listMoviesByYearHandler(req, res) {
+  const year = parseInt(req.params.year);
+
+  try {
+      const movies = listByYear(year);
+
+      if (movies.length === 0) {
+          return res.status(404).json({ message: 'Year not found' });
+      }
+
+      res.status(200).json(movies);
+  } catch (error) {
+      res.status(500).json({ message: error.message });
+  }
+}
+
+export function deleteMovieHandler(req, res) {
+  const id = req.params.id;
+
+  try {
+      const deletedMovie = deleteById(id);
+
+      if (!deletedMovie) {
+          return res.status(404).json({ message: 'Movie not found' });
+      }
+
+      res.status(204).send();
+  } catch (error) {
+      res.status(500).json({ message: error.message });
+  }
+}
+
+
+export function updateMovieHandler(req, res) {
     try {
-        const updatedMovie = updateMovieById(id, data);
+        const updatedMovie = updateById(req.params.id, req.body);
         if (!updatedMovie) {
-            console.log('Movie not found, nothing to update.');
-            return;
+          return res.status(404).json({ message: "Movie not found" });
         }
-        console.log('Movie updated successfully:', updatedMovie);
-    } catch (error) {
-        console.error('Error updating movie:', error.message);
-    }
-};
+        res.status(200).json(updatedMovie);
+      } catch (error) {
+        res.status(500).json({ message: error.message });
+      }
+}
