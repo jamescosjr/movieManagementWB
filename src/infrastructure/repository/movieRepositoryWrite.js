fd49a394954f60ac454b3fac5a6a4ec37c0c37f0
jamescosjr <jamescosjr@gmail.com>
2025-01-05 12:40:27 -0300

    feat: implement create and update functions for movie repository

---
+++ src/infrastructure/repository/movieRepositoryWrite.js
@@ -0,0 +1,24 @@
+import { Movie }from '../schema/movieSchema.js';
+import { AppError } from '../../domain/error/customErros.js';
+
+export async function createMovie({ title, director, genre, year }) {
+    try {
+        const newMovie = new Movie({
+            title,
+            director,
+            genre,
+            year,
+        });
+        return await newMovie.save();
+    } catch (error) {
+        throw new AppError(error.message || 'Database error', 500);
+    }
+}
+
+export async function updateById(id, { title, director, genre, year }) {
+    try {
+        return await Movie.findByIdAndUpdate(id, { title, director, genre, year }, { new: true });
+    } catch (error) {
+        throw new AppError(error.message || 'Database error', 500);
+    }
+}
