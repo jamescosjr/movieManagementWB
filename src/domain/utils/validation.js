5644fa26a591394da19ceb579ccb22c6243593c6
jamescosjr <jamescosjr@gmail.com>
2025-01-05 12:40:32 -0300

    fix: update validation function to use 'genre' instead of 'gender'

--- src/domain/utils/validation.js
+++ src/domain/utils/validation.js
@@ -1,12 +1,12 @@
-export function validMovieData(title, director,gender, year) {
+export function validMovieData(title, director,genre, year) {
     if (typeof title !== 'string' || title.trim() === '') {
         return { valid: false, message: 'The title should be a valid string' };
     }
     if (typeof director !== 'string' || director.trim() === '') {
         return { valid: false, message: 'The director should be a valid string' };
     }
-    if (typeof gender !== 'string' || gender.trim() === '') {
-        return { valid: false, message: 'The gender should be a valid string' };
+    if (typeof genre !== 'string' || genre.trim() === '') {
+        return { valid: false, message: 'The genre should be a valid string' };
     }
     if (typeof year !== 'number' || year < 2000 || year > new Date().getFullYear()) {
         return { valid: false, message: 'The year should be a valid number' };
