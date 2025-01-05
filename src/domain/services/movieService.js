1109660c1e4e2ed4899a724346f8279dd2a4ef8b
jamescosjr <jamescosjr@gmail.com>
2025-01-05 12:40:37 -0300

    feat: add movie service functions for creating and updating movies

---
+++ src/domain/services/movieService.js
@@ -0,0 +1,21 @@
+import { 
+    createMovie,
+    updateById,
+ } from "../../infrastructure/repository/movieRepositoryWrite";
+import { AppError } from '../error/customErros'
+
+export async function createMovieService({ title, director, genre, year }) {
+    try {
+        return await createMovie({ title, director, genre, year });
+    } catch (error) {
+        throw new AppError(error.message || 'Error creating the movie', 500);
+    }
+}
+
+export async function updateMovieService(id, { title, director, genre, year }) {
+    try {
+        return await updateById(id, { title, director, genre, year });
+    } catch (error) {
+        throw new AppError(error.message || 'Error updating the movie', 500);
+    }
+};
