ec67f8c5b358a29af346e23cbd45b6e0074711b4
jamescosjr <jamescosjr@gmail.com>
2025-01-05 13:05:28 -0300

    feat: add deleteMovieService function for deleting movies

--- src/domain/services/movieService.js
+++ src/domain/services/movieService.js
@@ -1,8 +1,9 @@
 import { 
     createMovie,
     updateById,
- } from "../../infrastructure/repository/movieRepositoryWrite";
-import { AppError } from '../error/customErros'
+    deleteById,
+ } from "../../infrastructure/repository/movieRepositoryWrite.js";
+import { AppError } from '../error/customErros.js'
 
 export async function createMovieService({ title, director, genre, year }) {
     try {
@@ -19,3 +20,11 @@ export async function updateMovieService(id, { title, director, genre, year }) {
         throw new AppError(error.message || 'Error updating the movie', 500);
     }
 };
+
+export async function deleteMovieService(id) {
+    try {
+        return await deleteById(id);
+    } catch (error) {
+        throw new AppError(error.message || 'Error deleting the movie', 500);
+    }
+}
