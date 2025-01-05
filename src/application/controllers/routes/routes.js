e10b0159ebcc8c4a91e83dd3d84afc431e1839cc
jamescosjr <jamescosjr@gmail.com>
2025-01-05 13:05:33 -0300

    feat: enable movie deletion route in routes.js

--- src/application/controllers/routes/routes.js
+++ src/application/controllers/routes/routes.js
@@ -1,7 +1,8 @@
 import { Router } from "express";
 import {
   registerMovieHandler,
-  updateMovieHandler
+  updateMovieHandler,
+  deleteMovieHandler,
 } from "../movieController.js";
 
 const router = Router();
@@ -12,7 +13,7 @@ router.post("/movies", registerMovieHandler);
 // router.get("/movies/genre/:genre", listMoviesbyGenreHandler);
 // router.get("/movies/director/:director", listMoviesByDirectorHandler);
 // router.get("/movies/year/:year", listMoviesByYearHandler);
-// router.delete("/movies/:id", deleteMovieHandler);
+router.delete("/movies/:id", deleteMovieHandler);
 router.put("/movies/:id", updateMovieHandler);
 
 export default router;
