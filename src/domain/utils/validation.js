5cb4418820ed865c4da953c19920055e79c3ed4f
James Junior <jamescosjr@gmail.com>
2025-09-05 21:21:58 -0300

    fix: update year validation to allow years from 1000 onwards

--- src/domain/utils/validation.js
+++ src/domain/utils/validation.js
@@ -8,7 +8,7 @@ export function validMovieData(title, director,genre, year) {
     if (typeof genre !== 'string' || genre.trim() === '') {
         return { valid: false, message: 'The genre should be a valid string' };
     }
-    if (typeof year !== 'number' || year < 2000 || year > new Date().getFullYear()) {
+    if (typeof year !== 'number' || year < 1000 || year > new Date().getFullYear()) {
         return { valid: false, message: 'The year should be a valid number' };
     }
     return { valid: true };
