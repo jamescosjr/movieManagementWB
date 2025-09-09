50a3218785ee730998075dfe64ef503a332a9b8a
James Junior <jamescosjr@gmail.com>
2025-09-09 11:18:09 -0300

    fix: prevent returning empty array for page number zero in getAllMoviesHandler

--- src/application/controllers/movieController.js
+++ src/application/controllers/movieController.js
@@ -71,6 +71,10 @@ export async function getAllMoviesHandler(req, res, next) {
         const page = parseInt(req.query.page, 10) || 1;
         const limit = parseInt(req.query.limit, 10) || 10;
 
+        if (page === 0) {
+            return []
+        }
+
         if (page < 1 || limit < 1) {
             return next(new ValidationError("Page and limit must be positive integers."));
         }
